60d99a932574f22d96e9daf3314f91766af3bdf9
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-20 08:09:32 -0300

    commit  changes

--- src/components/Header_Footer/footer.js
+++ src/components/Header_Footer/footer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import CityLogo from '../Utils/Button';
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
+    return(
+        <footer className="bck_blue">
+            <div className="footer_logo">
+                <CityLogo
+                    link={true}
+                    linkTo={'/'}
+                    width="70px"
+                    height="70px"
+                />
+            </div>
+            <div className="footer_descl">
+                Manchester city 2022.All rights reserved
+            </div>
+        </footer> 
+    )
 }
 
-export default Footer
+export default Footer;
