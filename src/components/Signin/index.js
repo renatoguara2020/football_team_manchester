0116e584f4716757221696b9d7b5259d5d384e0c
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-20 10:49:18 -0300

    commit changes

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,34 +1,81 @@
-import React,{useState} from 'react';
-import {CircularProgress} from '@material-ui/core';
-import {Redirect}  from 'react-router-dom';
-import {useFormik} from 'formik';
-import *  as Yup  from 'yup';
+import React , { useState } from 'react';
+
+import { CircularProgress } from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const SignIn = () => {
+    const [loading, setLoading] = useState(false)
 
     const formik = useFormik({
-
         initialValues:{
             email:'',
-            password: '',
+            password:''
         },
         validationSchema: Yup.object({
-        
-            email: Yup.string().email('Invalid email Address').required('This Email is Required!'),
-            password: '',
-
+            email: Yup.string()
+                .email('Invalid email address')
+                .required('The email is required'),
+            password:Yup.string()
+                .required('The password is required')
         }),
         onSubmit: (values) =>{
-
-
+            setLoading(true)
+          console.log(values)
         }
     })
 
-  return (
-    <h1>
-      <h3>Login</h3>
-    </h1>
-  )
+
+    return(
+        <div className="container">
+            <div className="signin_wrapper" style={{margin:'100px'}}>
+
+                <form onSubmit={formik.handleSubmit}>
+                    <h2>Please login</h2>
+                    <input
+                        name="email"
+                        placeholder="Enter your Email"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.email}
+                    />
+                    { formik.touched.email && formik.errors.email ?
+                        <div className="error_label">
+                            {formik.errors.email}
+                        </div>
+                    :null}
+
+
+                    <input
+                        name="password"
+                        placeholder="Enter your Password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.password}
+                    />
+                    { formik.touched.password && formik.errors.password ?
+                        <div className="error_label">
+                            {formik.errors.password}
+                        </div>
+                    :null}
+
+                    
+                        {loading ?
+                            <CircularProgress color="secondary" className="progress"/>
+                        :
+                            <button type="submit">Login</button>
+                        }
+
+                 
+                </form>
+
+            </div>
+        </div>
+    )
+
 }
 
 export default SignIn
