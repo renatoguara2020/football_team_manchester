5da9f721e6dc655e26c4571dcc194c32487b15fd
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-22 12:45:41 -0300

    commit changes

--- src/components/Home/featured/index.js
+++ src/components/Home/featured/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Test from './teste'
+//import Test from './teste'
 import Stripes from './Stripes';
+import Text from './Text';
 
 const Featured = () => {
   return (
     <div className="featured_wrapper">
         <Stripes />
+        <Text />
     </div>
   )
 }
